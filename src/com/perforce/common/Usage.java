9dff6f7bba820378f042b07e074678f20a647d67
Paul Allen <pallen@perforce.com>
2014-10-28 03:46:34 -0700

    Removed references to 'svn' from the name 'p4convert-svn'.
    Updated: Build, Usage, User docs, and Releases notes.
    
    Copied from Perforce
     Change: 10769

--- src/com/perforce/common/Usage.java
+++ src/com/perforce/common/Usage.java
@@ -8,17 +8,19 @@ public class Usage {
 	private static Logger logger = LoggerFactory.getLogger(Usage.class);
 
 	public static void print() {
-		logger.warn("No configuration file specified...\n");
+		logger.warn("No option or configuration file specified...\n");
 
 		if (logger.isInfoEnabled()) {
 			StringBuffer usage = new StringBuffer();
 			usage.append("Usage:\n");
-			usage.append("\tjava -jar p4convert-svn.jar <config file>\n\n");
-			usage.append("\tjava -jar p4convert-svn.jar --config [ SVN | CVS ]\n");
-			usage.append("\tjava -jar p4convert-svn.jar --version\n");
-			usage.append("\tjava -jar p4convert-svn.jar --info [ dumpfile ]\n");
-			usage.append("\tjava -jar p4convert-svn.jar --users [ dumpfile ]\n");
-			usage.append("\tjava -jar p4convert-svn.jar --extract [ rev ].[ node ] [ dumpfile ]\n");
+			usage.append("\tjava -jar p4convert.jar [ config file ]\n");
+			usage.append("\nOptions:\n");
+			usage.append("\tjava -jar p4convert.jar --config [ SVN | CVS ]\n");
+			usage.append("\tjava -jar p4convert.jar --version\n");
+			usage.append("\nSubversion specific options:\n");
+			usage.append("\tjava -jar p4convert.jar --info [ dumpfile ]\n");
+			usage.append("\tjava -jar p4convert.jar --users [ dumpfile ]\n");
+			usage.append("\tjava -jar p4convert.jar --extract [ rev ].[ node ] [ dumpfile ]\n");
 			logger.info(usage.toString());
 		}
 	}
