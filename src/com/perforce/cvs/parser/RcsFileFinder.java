831a3af8e56d24ffae50372003a611e6f75a3ff6
Paul Allen <pallen@perforce.com>
2015-04-08 06:39:38 -0700

    CVS: Rename badly encoded paths.
    First try CP1252, if that fails use a URI encoded path.  If a URI encoded path is used then it will be decode to UTF8.
    
    Copied from Perforce
     Change: 12260

--- src/com/perforce/cvs/parser/RcsFileFinder.java
+++ src/com/perforce/cvs/parser/RcsFileFinder.java
@@ -1,7 +1,17 @@
 package com.perforce.cvs.parser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -15,32 +25,76 @@ public class RcsFileFinder {
 	List<File> files = new ArrayList<File>();
 
 	public RcsFileFinder(String path) {
-		findFiles(path);
+		findFiles(new File(path));
 	}
 
-	private void findFiles(String path) {
-		File base = new File(path);
+	private void findFiles(File base) {
 		if (!base.exists()) {
-			logger.warn("CVSROOT does not exist: " + path);
+			logger.warn("CVSROOT does not exist: " + base);
 			return;
 		}
 
-		File[] list = base.listFiles();
+		DirectoryStream<Path> stream;
+		try {
+			stream = Files.newDirectoryStream(base.toPath());
 
-		for (File f : list) {
-			if (f.isDirectory()) {
-				if (!"CVSROOT".equals(f.getName())) {
-					findFiles(f.getAbsolutePath());
-				}
-			} else {
-				if (f.getName().endsWith(",v")) {
-					files.add(f);
-					logger.debug("file: " + f.getAbsolutePath());
-					count++;
-					System.out.print("Found: " + count + "\r");
+			Iterator<Path> iter = stream.iterator();
+			while (iter.hasNext()) {
+				Path path = iter.next();
+
+				if (path.toFile().isDirectory()) {
+					if (!"CVSROOT".equals(path.getFileName())) {
+						findFiles(path.toFile());
+					}
+				} else {
+					if (path.toString().endsWith(",v")) {
+						File file = verifyFile(path);
+						if (logger.isDebugEnabled()) {
+							logger.debug("file: " + file);
+						}
+						files.add(file);
+
+						count++;
+						System.out.print("Found: " + count + "\r");
+					}
 				}
 			}
+		} catch (IOException e) {
+			logger.error("Unable to list files: ", e);
 		}
+	}
+
+	private File verifyFile(Path path) {
+		File file = path.toFile();
+		try {
+			RandomAccessFile rf = new RandomAccessFile(file, "r");
+			try {
+				rf.close();
+			} catch (IOException e) {
+				logger.error("Unable to close file: ", file);
+			}
+		} catch (FileNotFoundException e) {
+			String uriStr = path.toUri().toString();
+			uriStr = uriStr.replaceFirst("file://", "");
+			
+			// re-encode path with a guess of CP1252, or fall back to URI.
+			// Windows (Western Europe code page) most commonly miss read!
+			try {
+				uriStr = URLDecoder.decode(uriStr, "windows-1252");
+				logger.debug("... decoding path with: windows-1252");
+			} catch (UnsupportedEncodingException e2) {
+				logger.warn("Unknown charset, encoding as URI");
+			}
+
+			file = new File(uriStr);
+			try {
+				logger.info("... renaming: " + file);
+				Files.move(path, file.toPath(), StandardCopyOption.ATOMIC_MOVE);
+			} catch (IOException e1) {
+				logger.error("Unable to rename file: " + file);
+			}
+		}
+		return file;
 	}
 
 	public List<File> getFiles() {
